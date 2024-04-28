2b76558a6d01d66778700a1a5ad3f4e4324c78a2
Diogo Alves <diogo-alves1998@hotmail.com>
2024-04-28 20:58:08 +0100

    moving camera

--- n-body-simulation.js
+++ n-body-simulation.js
@@ -2,7 +2,7 @@
 function getRandomId() {
     return Math.random() * 100000;
 }
-
+const COLLISION_TOLERANCE_PERCENTAGE = 200
 
 class Vector {
     constructor(x,y){
@@ -63,7 +63,7 @@ class Planet {
     }
 
     collidingWithOther(otherPlanet) {
-        return this.position.diff(otherPlanet.position).magnitudeSquared() < (this.radius + otherPlanet.radius)**2
+        return this.position.diff(otherPlanet.position).magnitudeSquared() < (this.radius + otherPlanet.radius)**2 * (1+COLLISION_TOLERANCE_PERCENTAGE/100)
     }
 }
 
@@ -98,7 +98,7 @@ class Universe {
                     const newMass = planet1.mass + planet2.mass;
                     this.planets[indexPlanet1] = new Planet(
                         newMass, 
-                        planet1.position, 
+                        planet1.mass > planet2.mass ? planet1.position : planet2.position, 
                         planet1.velocity.scalarMultiplication(planet1.mass/newMass).add(planet2.velocity.scalarMultiplication(planet2.mass/newMass)), 
                         planet1.id)
                     this.planets[indexPlanet2] = null;
@@ -115,6 +115,14 @@ class Canvas {
         this.canvas.width = width;
         this.canvas.height = height;
         document.body.appendChild(this.canvas);
+        this.canvasAspectRatio = width/height;
+        this.canvasBounds = {
+            xMax: width,
+            xMin: 0,
+            yMax: height,
+            yMin: 0,
+        }
+        this.currentCameraBounds = null;
         this.clearCanvas();
     }
 
@@ -126,30 +134,161 @@ class Canvas {
 
     updateWithUniverse(universe) {
         this.clearCanvas();
+
+        const pointsToCoverInCamera = universe.planets.map(p => p.position);
+        pointsToCoverInCamera.push(new Vector(-10,-10))
+        pointsToCoverInCamera.push(new Vector(10,10))
+        if(this.currentCameraBounds != null) {
+            // pointsToCoverInCamera.push(new Vector(this.currentCameraBounds.xMin, this.currentCameraBounds.yMin))
+            // pointsToCoverInCamera.push(new Vector(this.currentCameraBounds.xMax, this.currentCameraBounds.yMax))
+        }
+
+        this.currentCameraBounds = this.getCameraBounds(pointsToCoverInCamera)
+        this.drawAxisAndGrid(this.currentCameraBounds);
         universe.planets.forEach(planet => {
-            this.drawPlanet(planet);
+            this.drawPlanet(planet, this.currentCameraBounds);
         })
+        
     }
 
-    drawPlanet(planet) {
-        // ctx.fillRect(planet.position.x - 5, planet.position.y - 5, 10, 10);
+    drawAxisAndGrid(cameraBounds) {
+        
+        let n = Number.POSITIVE_INFINITY;
+        let distanceBetweenGridLines = 50;
+        let min = 0;
+        while(n > 20) {
+            n = n/2;
+            distanceBetweenGridLines *= 2; 
+            min = Math.min(cameraBounds.xMin, cameraBounds.yMin)
+            let max = Math.max(cameraBounds.xMax, cameraBounds.yMax)
+            min = Math.round(min/distanceBetweenGridLines)*distanceBetweenGridLines
+            max = Math.round(max/distanceBetweenGridLines)*distanceBetweenGridLines
+            n = Math.round((max-min)/distanceBetweenGridLines);
+        }
+        for(let i = 0; i<=n; i++) {
+            const gridPointPositionRelativeToCamera = this.absoluteToRelativePosition(new Vector(min + i*distanceBetweenGridLines,min + i*distanceBetweenGridLines), cameraBounds);
+            const gridPointPositionInCanvas = this.relativeToAbsolutePosition(gridPointPositionRelativeToCamera, this.canvasBounds); 
+            this.drawAxisThoughPoint(gridPointPositionInCanvas, 'blue', true);
+        }
+        const originPositionRelativeToCamera = this.absoluteToRelativePosition(new Vector(0,0), cameraBounds);
+        const originPositionInCanvas = this.relativeToAbsolutePosition(originPositionRelativeToCamera, this.canvasBounds);
+        this.drawAxisThoughPoint(originPositionInCanvas, 'red', false);
+    }
+
+    drawAxisThoughPoint(pointInCanvas, color, dashed) {
+        let ctx = this.canvas.getContext("2d");
+        if(dashed){
+            ctx.setLineDash([5, 15]);
+        } else {
+            ctx.setLineDash([])
+        }
+
+        ctx.beginPath();
+        ctx.moveTo(0, pointInCanvas.y);
+        ctx.lineTo(this.canvas.width, pointInCanvas.y);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+        
+
+        ctx.beginPath();
+        ctx.moveTo(pointInCanvas.x, 0);
+        ctx.lineTo(pointInCanvas.x, this.canvas.height);
+        ctx.strokeStyle = color;
+        ctx.stroke();
+
+    }
+
+    // drawPlanet(planet) {
+    //     // ctx.fillRect(planet.position.x - 5, planet.position.y - 5, 10, 10);
+    //     let ctx = this.canvas.getContext("2d");
+    //     ctx.fillStyle = "white";
+    //     ctx.beginPath();
+    //     ctx.arc(planet.position.x, planet.position.y, planet.radius, 0, 2 * Math.PI, true);
+    //     ctx.fill();
+    // }
+
+    drawPlanet(planet, cameraBounds) {
+        const planetPositionRelativeToCamera = this.absoluteToRelativePosition(planet.position, cameraBounds);
+        const planetPositionInCanvas = this.relativeToAbsolutePosition(planetPositionRelativeToCamera, this.canvasBounds);
+        const planetRadiusInCanvas = planet.radius * this.canvas.height/(cameraBounds.yMax - cameraBounds.yMin)
         let ctx = this.canvas.getContext("2d");
         ctx.fillStyle = "white";
         ctx.beginPath();
-        ctx.arc(planet.position.x, planet.position.y, planet.radius, 0, 2 * Math.PI, true);
+        ctx.arc(planetPositionInCanvas.x, planetPositionInCanvas.y, planetRadiusInCanvas, 0, 2 * Math.PI, true);
         ctx.fill();
     }
 
+    absoluteToRelativePosition(vector, bounds) {
+        return new Vector(
+            (vector.x - bounds.xMin)/parseFloat(bounds.xMax - bounds.xMin),
+            (vector.y - bounds.yMin)/parseFloat(bounds.yMax - bounds.yMin),
+        )
+    }
+
+    relativeToAbsolutePosition(vector, bounds) {
+        return new Vector(
+            vector.x * (bounds.xMax - bounds.xMin) + bounds.xMin,
+            vector.y * (bounds.yMax - bounds.yMin) + bounds.yMin,
+        )
+    }
+
+    getCameraBounds(pointsToCover) {
+        const PADDING = 200;
+        const bounds = {
+            xMax: Number.NEGATIVE_INFINITY,
+            xMin: Number.POSITIVE_INFINITY,
+            yMax: Number.NEGATIVE_INFINITY,
+            yMin: Number.POSITIVE_INFINITY,
+        }
+        pointsToCover.forEach(point => {
+            if(point.x + PADDING > bounds.xMax) {
+                bounds.xMax = point.x + PADDING;
+            }
+            if(point.x - PADDING < bounds.xMin) {
+                bounds.xMin = point.x - PADDING;
+            }
+            if(point.y + PADDING > bounds.yMax) {
+                bounds.yMax = point.y + PADDING;
+            }
+            if(point.y - PADDING < bounds.yMin) {
+                bounds.yMin = point.y - PADDING;
+            }
+        })
+        if((bounds.xMax - bounds.xMin)/(bounds.yMax - bounds.yMin) > this.canvasAspectRatio) {
+            let yDelta = (bounds.xMax - bounds.xMin)/this.canvasAspectRatio;
+            const yDeltaMissing = yDelta - (bounds.yMax - bounds.yMin);
+            bounds.yMax = bounds.yMax + yDeltaMissing/2;
+            bounds.yMin = bounds.yMin - yDeltaMissing/2;
+        } else {
+            let xDelta = (bounds.yMax - bounds.yMin)*this.canvasAspectRatio;
+            const xDeltaMissing = xDelta - (bounds.xMax - bounds.xMin);
+            bounds.xMax = bounds.xMax + xDeltaMissing/2;
+            bounds.xMin = bounds.xMin - xDeltaMissing/2;
+        }
+        const inflatedBounds = {
+            xMax: bounds.xMax,//(bounds.xMax - bounds.xMin) * 0.5,
+            xMin: bounds.xMin, //(bounds.xMax - bounds.xMin) * 0.5,
+            yMax: bounds.yMax,//(bounds.yMax - bounds.yMin) * 0.5,
+            yMin: bounds.yMin,//(bounds.yMax - bounds.yMin) * 0.5,
+        }
+        return inflatedBounds;
+    }
 }
 
 let universe = new Universe();
-universe.addPlanet(new Planet(20000000, new Vector(500, 300), new Vector(0, -0.16)));
-universe.addPlanet(new Planet(20000000, new Vector(300, 300), new Vector(0, 0.16)));
-universe.addPlanet(new Planet(30000000, new Vector(1400, 700), new Vector(0, 0)));
+universe.addPlanet(new Planet(20000000, new Vector(-200, 0), new Vector(0, 0.16)));
+universe.addPlanet(new Planet(20000000, new Vector(200, 0), new Vector(0, -0.16)));
+universe.addPlanet(new Planet(20000000, new Vector(0, 0), new Vector(0.1, 0.1)));
 universe.addPlanet(new Planet(30000000, new Vector(100, 700), new Vector(0, 0)));
 universe.addPlanet(new Planet(30000000, new Vector(1700, 700), new Vector(0, 0)));
-universe.addPlanet(new Planet(30000000, new Vector(2200, 700), new Vector(0, 0)));
+// universe.addPlanet(new Planet(30000000, new Vector(2200, 700), new Vector(0, 0)));
 let canvas = new Canvas(1500, 700)
+
+const stopAllPlanets = () => {
+    universe.planets.forEach(planet => {
+        planet.velocity = new Vector(0,0)
+    })
+}
 
 let previousTimeStamp;
 const step = timeStamp => {
